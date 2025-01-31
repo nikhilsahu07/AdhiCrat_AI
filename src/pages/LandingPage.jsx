a37e26537efee6b4047fb8db4198bdf49de387c5
Zaid Hassan <zaidhassan251@gmail.com>
2025-01-31 11:45:56 +0530

    Update landing page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,17 +1,38 @@
 import React from "react";
 import { Link } from "react-router";
-import SplashCursor from "../Bits/SplashCursor/SplashCursor"
+import SplashCursor from "../Bits/SplashCursor/SplashCursor";
+import { Typography } from "@mui/material";
+import BlurText from "../Bits/BlurText/BlurText";
+import Squares from "../Bits/Squares/Squares";
+import ShinyText from "../Bits/Shinytext/ShinyText";
 
 function LandingPage() {
   return (
-    <div className="h-screen">
-      
-      LandingPage
-      <SplashCursor />
-      <Link to="/Auth/login" className="bg-blue-200">
-        Login
-      </Link>
-      <Link to="/app">Get Started</Link>
+    <div className="h-screen flex flex-col bg-black justify-center items-center gap-11">
+      <Squares
+        speed={0.5}
+        squareSize={40}
+        direction="diagonal" // up, down, left, right, diagonal
+        borderColor="#fff"
+        hoverFillColor="#222"
+      />
+
+      <div className="absolute z-10 gap-y-11 flex flex-col justify-center items-center">
+      <BlurText
+          text="LegisAI"
+          delay={150}
+          animateBy="letters"
+          direction="bottom"
+          // onAnimationComplete={handleAnimationComplete}
+          className="text-9xl text-white"
+        />
+        {/* </Typography> */}
+        <Link to="/app" className="bg-gray-800 px-4 py-2">
+          <ShinyText text="Get Started" disabled={false} speed={3} className='' />
+        </Link>
+      </div>
+
+      {/* <SplashCursor /> */}
     </div>
   );
 }
