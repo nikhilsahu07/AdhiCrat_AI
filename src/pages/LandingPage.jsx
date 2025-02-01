a01bdcae21048d0db8f3e00f1f307b2f7ca31438
Zaid Hassan <zaidhassan251@gmail.com>
2025-02-01 21:59:26 +0530

    update landing page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,40 +1,114 @@
 import React from "react";
 import { Link } from "react-router";
-import SplashCursor from "../Bits/SplashCursor/SplashCursor";
-import { Typography } from "@mui/material";
 import BlurText from "../Bits/BlurText/BlurText";
-import Squares from "../Bits/Squares/Squares";
 import ShinyText from "../Bits/Shinytext/ShinyText";
-import { ArrowRightAlt } from "@mui/icons-material";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import CardActionArea from "@mui/material/CardActionArea";
+
+const cards = [
+  {
+    id: 1,
+    title: "Voice Dictation",
+    description: "Plants are essential for all life.",
+    route: "productivity/voice-dictation",
+  },
+  {
+    id: 2,
+    title: "Task Reminder",
+    description: "Animals are a part of nature.",
+    route: "productivity/task-reminder",
+  },
+  {
+    id: 3,
+    title: "Translation",
+    description: "Humans depend on plants and animals for survival.",
+    route: "productivity/translation",
+  },
+  {
+    id: 4,
+    title: "AI Writer",
+    description: "Humans depend on plants and animals for survival.",
+    route: "productivity/ai-writer",
+  },
+];
 
 function LandingPage() {
+  const [selectedCard, setSelectedCard] = React.useState(0);
   return (
-    <div className="h-screen flex flex-col bg-[#e1e1e1] justify-center items-center gap-11">
-      {/* <Squares
-        speed={0.5}
-        squareSize={40}
-        direction="diagonal" // up, down, left, right, diagonal
-        borderColor="#fff"
-        hoverFillColor="#222"
-      /> */}
-
-      {/* <div className="absolute z-10 gap-y-11 flex flex-col justify-center items-center"> */}
-      <BlurText
+    <>
+      <section className="min-h-screen flex flex-col bg-[#e1e1e1] justify-center items-center gap-11">
+        <BlurText
           text="ADHICRAT"
           delay={150}
           animateBy="letters"
           direction="bottom"
           // onAnimationComplete={handleAnimationComplete}
-          className="text-9xl text-[#1e1e1e] samarkan"
+          className="text-6xl md:text-9xl text-[#1e1e1e] samarkan"
         />
-        {/* </Typography> */}
-        <Link to="/app" className="bg-gray-800 px-4 py-2">
-          <ShinyText text="Get Started" disabled={false} speed={3} className='' />
+        <Link
+          to="/app"
+          className="bg-zinc-900 border-2 border-zinc-400 px-4 py-2 rounded-4xl"
+        >
+          <ShinyText
+            text="Get Started"
+            disabled={false}
+            speed={3}
+            className="text-sm md:text-lg"
+          />
         </Link>
-      {/* </div> */}
-      
-      <SplashCursor />
-    </div>
+        {/* <SplashCursor /> */}
+      </section>
+      <section className="h-screen p-6 bg-[#1e1e1e] text-[#e1e1e1] flex flex-col items-center justify-center gap-y-11">
+          <Typography variant="h4">Productivity</Typography>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              width: "100%",
+            }}
+            >
+            <Box
+              sx={{
+                display: "grid",
+                gridTemplateColumns: "repeat(auto-fit, minmax(400px, 1fr))",
+                gap: 4,
+              }}
+              className="w-[100%] md:w-[50%]"
+            >
+              {cards.map((card, index) => (
+                <Card sx={{ display: "flex", justifyContent: "center" }}>
+                  <CardActionArea
+                    onClick={() => setSelectedCard(index)}
+                    data-active={selectedCard === index ? "" : undefined}
+                    sx={{
+                      height: "100%",
+                      "&[data-active]": {
+                        backgroundColor: "action.selected",
+                        "&:hover": {
+                          backgroundColor: "action.selectedHover",
+                        },
+                      },
+                    }}
+                  >
+                    <CardContent sx={{ height: "100%" }}>
+                      <Typography variant="h5" component="div">
+                        {card.title}
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        {card.description}
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+              ))}
+            </Box>
+          </Box>
+      </section>
+    </>
   );
 }
 
